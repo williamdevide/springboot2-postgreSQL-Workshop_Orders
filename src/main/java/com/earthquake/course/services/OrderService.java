d7b02cb1a60646a467621b8d746d9060ef92ec04
William Devide Komel <williamdevide@gmail.com>
2023-10-30 11:40:03 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/earthquake/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.earthquake.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.earthquake.course.entities.Order;
+import com.earthquake.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
